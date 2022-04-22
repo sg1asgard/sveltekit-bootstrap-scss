847bc53544497dc6f2edb4e6812b63558c424605
George A <sg1asgard@outlook.com>
2022-04-22 09:40:48 +0300

    scss setup

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button type="button" class="btn btn-primary">Primary</button>
+
+<style lang="scss">
+    @import "../scss/main.scss";
+    h1 { color: $primary; }
+</style>
